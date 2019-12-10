de1b574ed29e18e2754e1991c7ba3410fab49f95
Flavio Adrián Andrada Trainini <andrada.flavio@outlook.com>
2019-12-10 02:04:59 -0300

    ..

--- src/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/api/books',require('./books_routes'))
-app.use('/api/user',require('./user_routes'))
-app.use('/api/login',require('./login_routes'))
-
-module.exports = app
